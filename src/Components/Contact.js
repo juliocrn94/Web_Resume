67b749770e2192c67bde4d6894ea69dd92e93405
julio <julio@iMac-de-julio.local>
2020-03-17 23:43:05 -0600

    contact form done

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,10 +5,8 @@ class Contact extends Component {
 
     if(this.props.data){
       var name = this.props.data.name;
-      var street = this.props.data.address.street;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
+      var linkedin = this.props.data.linkedin;
+      var github = this.props.data.github;
       var phone= this.props.data.phone;
       var email = this.props.data.email;
       var message = this.props.data.contactmessage;
@@ -78,36 +76,16 @@ class Contact extends Component {
             <aside className="four columns footer-widgets">
                <div className="widget widget_contact">
 
-					   <h4>Address and Phone</h4>
+					   <h4>Other Contact Info:</h4>
 					   <p className="address">
 						   {name}<br />
-						   {street} <br />
-						   {city}, {state} {zip}<br />
-						   <span>{phone}</span>
+                     <a href="http://bit.ly/33eNmmX" target="_blank" rel="noopener noreferrer"><span>{phone}</span><br /></a>
+						   <a href="#contact" target="_blank" rel="noopener noreferrer"><span>{email}</span><br /></a>
+						   <a href="http://bit.ly/2Q8bceW" target="_blank" rel="noopener noreferrer"><span>{linkedin}</span><br /></a>
+						   <a href="http://bit.ly/38IBjj9" target="_blank" rel="noopener noreferrer"><span>{github}</span></a>
 					   </p>
 				   </div>
 
-               <div className="widget widget_tweets">
-                  <h4 className="widget-title">Latest Tweets</h4>
-                  <ul id="twitter">
-                     <li>
-                        <span>
-                        This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
-                        Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum
-                        <a href="#">http://t.co/CGIrdxIlI3</a>
-                        </span>
-                        <b><a href="#">2 Days Ago</a></b>
-                     </li>
-                     <li>
-                        <span>
-                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-                        eaque ipsa quae ab illo inventore veritatis et quasi
-                        <a href="#">http://t.co/CGIrdxIlI3</a>
-                        </span>
-                        <b><a href="#">3 Days Ago</a></b>
-                     </li>
-                  </ul>
-		         </div>
             </aside>
       </div>
    </section>
